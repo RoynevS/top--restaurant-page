7637fdf0d3065fb99daabe28b29eed5f942ab049
RoynevS <109765130+RoynevS@users.noreply.github.com>
2024-04-07 22:45:26 +0200

    Add css and image loader on top of a source map

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,22 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
   devServer: {
     static: "./dist",
   },
+  devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
       title: "Restaurant-Page",

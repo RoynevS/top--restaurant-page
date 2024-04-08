2176f5a1172a585b8e9494c3f52a1e26ad4bbf10
RoynevS <109765130+RoynevS@users.noreply.github.com>
2024-04-08 09:23:11 +0200

    Remove footer

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./styles/general-style.css";
 function loadPage() {
   const contentWrapper = document.querySelector("#content");
   contentWrapper.appendChild(createHomepage().main());
-  contentWrapper.appendChild(createHomepage().footer());
 }
 
 loadPage();

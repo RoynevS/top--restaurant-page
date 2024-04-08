cdd4c097efa108ddde1487f11272f46a22e98e00
RoynevS <109765130+RoynevS@users.noreply.github.com>
2024-04-08 11:02:39 +0200

    Switch content on button click

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import createHomepage from "./initial-page";
+import menuPage from "./menu";
 import "./styles/general-style.css";
 
 function loadPage() {
   const contentWrapper = document.querySelector("#content");
-  contentWrapper.appendChild(createHomepage().main());
+  const homeBtn = document.querySelector(".home-btn");
+  const menuBtn = document.querySelector(".menu-btn");
+  const aboutBtn = document.querySelector(".about-btn");
+  
+  const homepage = () => {
+    const main = document.querySelector("main");
+    if (contentWrapper.children.length === 0) {
+      contentWrapper.appendChild(createHomepage().main());
+    } else {
+      contentWrapper.replaceChild(createHomepage().main(), main);
+    }
+  }
+
+  const menuTab = () => {
+    const main = document.querySelector("main");
+    contentWrapper.replaceChild(menuPage().main(), main);
+  }
+  
+  homeBtn.addEventListener("click", homepage);
+  
+  menuBtn.addEventListener("click", menuTab);
+
+  homepage();
 }
 
 loadPage();
